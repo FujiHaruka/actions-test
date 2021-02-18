43b8ff72341f1c397141e4d681e024d504db116d
FujiHaruka <fizzyjohn1427@gmail.com>
2021-02-18 11:52:47 +0900

    Fix action

--- scripts/label.js
+++ scripts/label.js
@@ -4,18 +4,19 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function main() {
+  const {
+    context: {
+      repo: { owner, repo },
+    },
+  } = github;
+  const { REF_ISSUES = "", GITHUB_TOKEN = "" } = process.env;
   try {
-    const issues = core
-      .getInput("issues")
-      .split(",")
-      .map(Number)
-      .filter(Boolean);
-    const token = core.getInput("token");
-    const octokit = github.getOctokit(token);
+    const issues = REF_ISSUES.split(",").map(Number).filter(Boolean);
+    const octokit = github.getOctokit(REF_ISSUES);
     for (const issue of issues) {
       await octokit.issues.addLabels({
-        owner: "FujiHaruka",
-        repo: "actions-test",
+        owner,
+        repo,
         issue_number: issue,
         labels: "pr-merged",
       });
